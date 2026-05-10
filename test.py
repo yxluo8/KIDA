93232a6cbeee02963df0c82482e733da6225b645
yxluo8 <yuxuanluo@hust.edu.cn>
2026-05-10 10:21:36 +0800

    Update test.py

--- test.py
+++ test.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-wp", "--weight_path", required=True, type=str, help="Path to best_model_weight.pth")
     parser.add_argument("-exp", default="KIDA_EVAL", type=str)
-    parser.add_argument("-dp", "--data_path", default="/memory/luoyuxuan4090/SEDMamba/data", type=str)
+    parser.add_argument("-dp", "--data_path", default="./data", type=str)
     parser.add_argument("-gpu_id", default="cuda:0", type=str)
     parser.add_argument("-w", "--work", default=4, type=int)
     parser.add_argument("-et","--emb_type", default='dinoemb', type=str)
